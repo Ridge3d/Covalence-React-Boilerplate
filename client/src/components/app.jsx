f717e4cb7f8fdfc17de3702e115f0d4148c90b59
Jacey Edwards <jaceyedwards@Jaceys-MacBook-Pro.local>
2018-02-16 10:35:26 -0600

    Starting Authentication

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,14 +2,21 @@ import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import FrontPage from "./frontpage";
 import PostPage from "./postpage";
+import PrivateRoute from "./auth/privateRoute";
+import Login from "./auth/login";
+import Logout from "./auth/logout";
+import AuthButton from "./auth/authButton";
 
 class Navigation extends Component {
   render() {
     return (
       <Router>
         <Fragment>
+          <AuthButton />
           <Switch>
             <Route exact path="/" component={FrontPage} />
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout} />
             <Route exact path="/post" component={PostPage} />
           </Switch>
         </Fragment>
